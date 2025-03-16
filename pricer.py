98a20d54033fb986e6f5b6ecb16d7e99abe57250
AlirezV <ggh78620@gmail.com>
2025-03-16 21:18:56 +0330

    connecting to database is a function now

--- pricer.py
+++ pricer.py
@@ -2,42 +2,41 @@ import argparse
 import re
 import psycopg2
 
-#test
+def connect():
+    conn = psycopg2.connect( user="postgres", password="1234", database="postgres")
+    return conn
 
 def add_item(items, date, name, price):
     items[date] = (name, price)
     return items
 
-def save_to_psql(items):
-    
+def save_to_psql(items, conn):
     valied_names = ["bitcoin", "dogecoin", "dollar_toman", "ethereum", "seke_toman"]
     if items[date][0] not in valied_names:
         print("Name is not correct!")        
     else:
         try:
-            conn = psycopg2.connect( user="postgres", password="1234", database="postgres")
-            cursor = conn.cursor()
+            cursor = conn.cursor() 
             for date, (name, price) in items.items():
                 cursor.execute("insert into %s values ('%s', '%s', %s );" %(name, date, name, price ))
-            conn.commit()
+            conn.commit() 
             cursor.close()
             conn.close()
         except psycopg2.Error as e:
             print("database error: %s" % e)
     
-def load_from_psql(table_name):
+def load_from_psql(table_name, conn):
     items = {}
     valied_tables = ["bitcoin", "dogecoin", "dollar_toman", "ethereum", "seke_toman"]
     if table_name not in valied_tables:
         print("Table name is not correct!")        
     try:
-        conn = psycopg2.connect( user="postgres", password="1234", database="postgres")
         cursor = conn.cursor()
         cursor.execute("select * from %s;" % table_name)
         iitems = cursor.fetchall()
         for row in iitems:
-            time, name, price = row
-            date = re.sub(r'(.*)-(.*)-(.*)',r'\1/\2/\3',time)
+            date, name, price = row
+            #date = re.sub(r'(.*)-(.*)-(.*)',r'\1/\2/\3',time)
             items[date] = (name , price)
         cursor.close()
         conn.close()
@@ -87,18 +86,13 @@ def main():
     parser.add_argument("--reverse", action="store_true", help="sort in descending order")
 
     args = parser.parse_args()
-    items = load_from_psql(args.name) if args.name else {}
+    conn = connect()
+    items = load_from_psql(args.name, conn) if args.name else {}
 
-    if args.action == "price":
-        if args.name and args.date:
-            price_item(items, args.date, args.name)
-        else:
-            print("please provide both --name and --date!")
-    
     if args.action == "add":
         if args.date and args.name and args.price is not None:
             items = add_item(items, args.date, args.name, args.price)
-            save_to_psql(items)
+            save_to_psql(items, conn)
             print(f"item: '{args.name}' at the price {args.price} on date of {args.date} added.")
         else:
             print("please insert name, price and date of currency.")
@@ -113,6 +107,12 @@ def main():
         sort_items(items, args.reverse)
     elif args.action == "report":
         report(items)
+    elif args.action == "price":
+        if args.name and args.date:
+            price_item(items, args.date, args.name)
+        else:
+            print("please provide both --name and --date!")
+    
 
 if __name__ == "__main__":
     main()
